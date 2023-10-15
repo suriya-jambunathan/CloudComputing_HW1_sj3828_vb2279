5bfce5c0afdb49de797f4e7e3d22c046190955b3
Vamsi Krishna Bunga <vamsikrishh0099@gmail.com>
2023-10-15 18:39:29 -0400

    frontend files and ES data file

---
+++ frontend/assets/js/chat.js
@@ -0,0 +1,151 @@
+var checkout = {};
+
+function generateRandomString(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
+}
+
+function createSessionId() {
+  // Generate a random 10-character string
+  const randomString = generateRandomString(10);
+
+  // Generate the timestamp when the conversation starts
+  const timestamp = new Date().toISOString();
+
+  // Combine the timestamp and the random string
+  const sessionId = `${timestamp}-${randomString}`;
+
+  return sessionId;
+}
+
+// Create a session ID when the conversation starts
+const sessionId = createSessionId();
+console.log(`Session ID: ${sessionId}`);
+
+if (document.cookie === '') {
+  document.cookie = sessionId;
+}
+
+// Use the same session ID for all user responses during this conversation
+
+
+$(document).ready(function() {
+  var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+  $(window).load(function() {
+    $messages.mCustomScrollbar();
+    insertResponseMessage('Hi there, I\'m your personal Concierge. How can I help?');
+  });
+
+  function updateScrollbar() {
+    $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+      scrollInertia: 10,
+      timeout: 0
+    });
+  }
+
+  function setDate() {
+    d = new Date()
+    if (m != d.getMinutes()) {
+      m = d.getMinutes();
+      $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    }
+  }
+
+  function callChatbotApi(message) {
+    // params, body, additionalParams
+    return sdk.chatbotPost({}, {
+      messages: [{
+        type: 'unstructured',
+        unstructured: {
+          text: message,
+          id: document.cookie
+        }
+      }]
+    }, {});
+  }
+
+  function insertMessage() {
+    msg = $('.message-input').val();
+    if ($.trim(msg) == '') {
+      return false;
+    }
+    $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    $('.message-input').val(null);
+    updateScrollbar();
+
+    callChatbotApi(msg)
+      .then((response) => {
+        console.log(response);
+        var data = response.data;
+
+        if (data.messages && data.messages.length > 0) {
+          console.log('received ' + data.messages.length + ' messages');
+
+          var messages = data.messages;
+
+          for (var message of messages) {
+            if (message.type === 'unstructured') {
+              insertResponseMessage(message.unstructured.text);
+            } else if (message.type === 'structured' && message.structured.type === 'product') {
+              var html = '';
+
+              insertResponseMessage(message.structured.text);
+
+              setTimeout(function() {
+                html = '<img src="' + message.structured.payload.imageUrl + '" witdth="200" height="240" class="thumbnail" /><b>' +
+                  message.structured.payload.name + '<br>$' +
+                  message.structured.payload.price +
+                  '</b><br><a href="#" onclick="' + message.structured.payload.clickAction + '()">' +
+                  message.structured.payload.buttonLabel + '</a>';
+                insertResponseMessage(html);
+              }, 1100);
+            } else {
+              console.log('not implemented');
+            }
+          }
+        } else {
+          insertResponseMessage('Oops, something went wrong. Please try again.');
+        }
+      })
+      .catch((error) => {
+        console.log('an error occurred', error);
+        insertResponseMessage('Oops, something went wrong. Please try again.');
+      });
+  }
+
+  $('.message-submit').click(function() {
+    insertMessage();
+  });
+
+  $(window).on('keydown', function(e) {
+    if (e.which == 13) {
+      insertMessage();
+      return false;
+    }
+  })
+
+  function insertResponseMessage(content) {
+    $('<div class="message loading new"><figure class="avatar"><img src="https://media.tenor.com/images/4c347ea7198af12fd0a66790515f958f/tenor.gif" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+    updateScrollbar();
+
+    setTimeout(function() {
+      $('.message.loading').remove();
+      $('<div class="message new"><figure class="avatar"><img src="https://media.tenor.com/images/4c347ea7198af12fd0a66790515f958f/tenor.gif" /></figure>' + content + '</div>').appendTo($('.mCSB_container')).addClass('new');
+      setDate();
+      updateScrollbar();
+      i++;
+    }, 500);
+  }
+
+});
